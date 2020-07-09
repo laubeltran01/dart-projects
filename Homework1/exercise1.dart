fbbb49e189b8234bd2d34848c59af56d6dc6ddc6
Sebastian Leal <sebastianleaj10@gmail.com>
2020-07-08 19:21:12 -0500

    Folder

---
+++ Homework1/exercise1.dart
@@ -0,0 +1,20 @@
+void main() {
+  
+  //Introduce your age here
+  var age = 21;
+  
+  if (age < 18)
+  {
+    print("minor");
+  }
+  
+  if (age >= 18 && age <= 20)
+  {
+    print("Legal age");
+  }
+  
+  if (age > 20)
+  {
+    print("citizen");
+  }
+}
